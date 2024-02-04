a7bc5b04c613ee9e48460abf34e01d8efa70c108
Angkhana <34016307+angkhanalil@users.noreply.github.com>
2024-02-04 17:54:05 +0700

    del file not use

--- index.js
+++ index.js
@@ -10,40 +10,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/v1/todos", todosRoutes);
-
-const longestCommonPrefix = (arr) => {
-  if (arr.length >= 1 && arr.length <= 200) {
-    let result = arr[0].toLowerCase().trim();
-    let length = result.length;
-
-    if (arr.every((obj) => obj.length > 0 && obj.length <= 200)) {
-      for (let i = 1; i < arr.length; i++) {
-        while (arr[i].toLowerCase().trim().indexOf(result) !== 0) {
-          result = result.substring(0, length - 1);
-          length--;
-          if (result === "") {
-            return "There is no common prefix among the input strings.";
-          }
-        } 
-      }
-      return result;
-    } else {
-      return "string is too long.";
-    }
-  }else{
-    return "length  of string should be more than or equal 1 and less than or equal 200.";
-  }
-};
-
+ 
 app.get("/hi", (req, res) => {
   res.send("Hello, World!");
-
-  const input = [
-    "Flower",
-    "flow",
-    " flight",
-  ];
-  console.log(longestCommonPrefix(input));
+ 
 });
 
 // 404 Error
